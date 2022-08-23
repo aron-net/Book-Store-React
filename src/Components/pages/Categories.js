fc907d7bf2434d523c4871ea5958bfcffbcce0cf
aron-helu <aronmilahela@gmail.com>
2022-08-23 16:12:20 +0300

    add dispatch checkstatus

--- src/Components/pages/Categories.js
+++ src/Components/pages/Categories.js
@@ -1,7 +1,15 @@
-const Categories = () => (
-  <div>
-    <button type="button">Check status</button>
-  </div>
-);
+import { useDispatch } from "react-redux";
+import { checkStatus } from "../../redux/Categories/categories";
+const Categories = () => {
+  const dispatch = useDispatch();
+  const checkHandler = () => {
+    dispatch(checkStatus());
+  }
+  return (
+    <div>
+      <button type="button" onClick={checkHandler}>Check status</button>
+    </div>
+  );
+}
 
 export default Categories;
