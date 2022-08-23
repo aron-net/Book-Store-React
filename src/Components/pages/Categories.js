8a4410853813969cc8c66874cfa855da9c03755d
aron-helu <aronmilahela@gmail.com>
2022-08-23 16:17:52 +0300

    fix linters

--- src/Components/pages/Categories.js
+++ src/Components/pages/Categories.js
@@ -1,15 +1,16 @@
-import { useDispatch } from "react-redux";
-import { checkStatus } from "../../redux/Categories/categories";
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../../redux/Categories/categories';
+
 const Categories = () => {
   const dispatch = useDispatch();
   const checkHandler = () => {
     dispatch(checkStatus());
-  }
+  };
   return (
     <div>
       <button type="button" onClick={checkHandler}>Check status</button>
     </div>
   );
-}
+};
 
 export default Categories;
