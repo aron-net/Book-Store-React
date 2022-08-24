899bd00381ecba19ee7f5154c7cc119ab391aed3
aron-helu <aronmilahela@gmail.com>
2022-08-24 23:01:44 +0300

    connect to api with redux

--- src/Components/pages/Categories.js
+++ src/Components/pages/Categories.js
@@ -1,14 +1,18 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { checkStatus } from '../../redux/Categories/categories';
+import './category.css';
 
 const Categories = () => {
   const dispatch = useDispatch();
+  const categories = useSelector((state) => state.categories, shallowEqual);
   const checkHandler = () => {
     dispatch(checkStatus());
   };
+  const Button = <button className="category-button" type="button" onClick={checkHandler}>Check status</button>;
   return (
     <div>
-      <button type="button" onClick={checkHandler}>Check status</button>
+      <h2 className="category-title">{categories}</h2>
+      {Button}
     </div>
   );
 };
