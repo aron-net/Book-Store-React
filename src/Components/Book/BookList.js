05e731cbc434b36dc4ff36b656d43efe51fc0148
aron-helu <aronmilahela@gmail.com>
2022-08-26 00:08:36 +0300

    style book store

--- src/Components/Book/BookList.js
+++ src/Components/Book/BookList.js
@@ -4,16 +4,52 @@ import { removeBook } from '../../redux/Book/book';
 import './BookList.css';
 
 const BookList = (props) => {
-  const { id, title, author } = props;
+  const {
+    id,
+    title,
+    author,
+    category,
+  } = props;
+  const randomPercentge = Math.floor(Math.random() * (100 - 0 + 1) + 0);
+  const randomChapter = Math.floor(Math.random() * (20 - 0 + 1) + 0);
   const dispatch = useDispatch();
   const clickHandler = () => {
     dispatch(removeBook(id));
   };
   return (
     <li>
-      <h2>{title}</h2>
-      <p>{author}</p>
-      <button className="aro" type="button" onClick={clickHandler}>Remove Book</button>
+      <div className="book-card">
+        <h4 className="category">{category}</h4>
+        <h2 className="title">{title}</h2>
+        <h6 className="author">{author}</h6>
+        <div className="btn">
+          <p className="comment">Comments</p>
+          <button className="removeBtn" type="button" onClick={clickHandler}>Remove Book</button>
+          <p className="edit">Edit</p>
+        </div>
+      </div>
+      <div className="book-progress">
+        <div className="progress-container">
+          <div className="circular-progress-container">
+            <div role="progressbar" aria-label="progress Bar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style={{ '--value': randomPercentge }} />
+          </div>
+          <div className="progress-stat">
+            <p className="percent-complete">
+              {`${randomPercentge}%`}
+            </p>
+            <p className="completed">Completed</p>
+          </div>
+        </div>
+      </div>
+      <div className="current-chapter-container">
+        <div>
+          <p className="current-chapter-label">CURRENT CHAPTER</p>
+          <p className="current-chapter">
+            {`Chapter ${randomChapter}`}
+          </p>
+        </div>
+        <div><button className="primary-button" type="button">UPDATE PROGRESS</button></div>
+      </div>
     </li>
   );
 };
@@ -22,5 +58,6 @@ BookList.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
 };
 export default BookList;
--- src/Components/NewBook/NewBook.js
+++ src/Components/NewBook/NewBook.js
@@ -4,17 +4,20 @@ import { v4 as rid } from 'uuid';
 import { addBook } from '../../redux/Book/book';
 import './NewBook.css';
 
+const options = ['Action', 'Science Fiction', 'Economy'];
+
 const NewBook = () => {
   const dispatch = useDispatch();
   const [titleName, setTitle] = useState('');
   const [authorName, setAuthor] = useState('');
+  const [selected, setSelected] = useState(options[0]);
   const addBookHandler = (e) => {
     e.preventDefault();
     const book = {
       id: rid(),
       title: titleName,
       author: authorName,
-      category: 'not categorized yet',
+      category: selected,
     };
     dispatch(addBook(book));
     setTitle('');
@@ -22,10 +25,17 @@ const NewBook = () => {
   };
   return (
     <section className="ar">
-      <h2>ADD NEW BOOK</h2>
+      <h2 className="form-title">ADD NEW BOOK</h2>
       <form onSubmit={addBookHandler}>
         <input type="text" value={titleName} onInput={(e) => setTitle(e.target.value)} placeholder="Book title" required="" />
         <input type="text" value={authorName} onInput={(e) => setAuthor(e.target.value)} placeholder="Author" required="" />
+        <select className="category-input" value={selected} onChange={(e) => setSelected(e.target.value)}>
+          {options.map((value) => (
+            <option value={value} key={value}>
+              {value}
+            </option>
+          ))}
+        </select>
         <button type="submit">ADD BOOK</button>
       </form>
     </section>
